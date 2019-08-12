8a7da5db42ac46d57bcd3081ba9983b3ae28c482
Greg Roques <greg.roques@gmail.com>
2019-08-12 12:21:20 -0400

    update

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,22 +1,22 @@
 import React from "react";
-import update from './update.module.css'
+import update from "./update.module.css";
 
 const ResultModal = props => {
     const cssModal = [
         update.Modal,
         props.show ? update.ModalOpen : update.ModalClosed
     ];
-  return (
-    <div className={cssModal.join(' ')}>
-        <div>
-            <p>Your trimmed URL is:</p>
-            <p><a rel="noopener noreferrer" href={props.yourHref} target="_blank" >{props.newURL}</a></p>
+    return (
+        <div className={cssModal.join(" ")}>
+            <div>
+                <p>Your trimmed URL is:</p>
+                <p><a rel="noopener noreferrer" href={props.yourHref} target="_blank" >{props.newURL}</a></p>
+            </div>
+            <button className={update.closeButton} onClick={props.closed}>
+                x
+            </button>
         </div>
-        <button className={update.closeButton} onClick={props.closed}>
-            x
-        </button>
-    </div>
-  );
+    );
 };
 
 export default ResultModal;
