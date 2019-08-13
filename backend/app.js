bd9fa14ab38e27ed9b7e709e3d6873302cb6ac04
Greg Roques <greg.roques@gmail.com>
2019-08-13 14:35:43 -0400

    thus far

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,15 @@
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+const PORT = 2000;
 
-app.use((req, res, next)=>{
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
 
 });
 
-app.listen(2000);
+app.listen(PORT, () => {
+    console.log("Listening on ", PORT);
+});
