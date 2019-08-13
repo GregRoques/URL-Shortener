4c806797342b469e13f9d90149f7894a7e7c1e88
Greg Roques <greg.roques@gmail.com>
2019-08-13 12:48:23 -0400

    backend

--- src/components/Redirector.js
+++ src/components/Redirector.js
@@ -1,8 +1,11 @@
 import React, { Component } from "react";
+import axios from "axios";
+import baseUrl from "./FirebaseUrl";
 
 export default class Redirector extends Component {
     componentDidMount () {
         const UrlHash = window.location.pathname.replace(/[/]/, "");
+        axios.get
     }
 
     render () {
