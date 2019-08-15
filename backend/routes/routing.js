959f19e1f9414bbdc417933d197aea34140543a2
Greg Roques <greg.roques@gmail.com>
2019-08-15 19:14:13 -0400

    finishing touches tomorrow

--- backend/routes/routing.js
+++ backend/routes/routing.js
@@ -2,6 +2,11 @@ const express = require("express");
 const router = express.Router();
 const db = require("../util/database");
 
+let newHash = "";
+const hasher = () => {
+    newHash = "create new hash, check its not a duplicate, assign";
+};
+
 router.get("/:hash", (req, res) => {
     const hashSearch = req.params.hash;
     const selectUrl = `SELECT url FROM urls WHERE hash='${hashSearch}'`;
@@ -15,13 +20,20 @@ router.get("/:hash", (req, res) => {
     });
 });
 
-router.post("/newurl", (req, res) => {
+router.post("/newurl", (req, res, next) => {
     const originalUrl = req.query.originalurl;
     const urlSearch = `SELECT hash, url FROM urls WHERE url='${originalUrl}'`;
     db.execute(urlSearch).then(results => {
         console.log(results[0][0]);
         res.json(results[0][0]);
+    }).catch(async () => {
+        await hasher();
+        next();
     });
+});
+
+router.post("/newurl", (req, res) => {
+    console.log(newHash);
 });
 
 module.exports = router;
